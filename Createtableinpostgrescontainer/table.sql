1fb14fe1b20cb3d9705dc51e35402d820d953814
Ahmed Bayoumii <125226127+Baioumi7@users.noreply.github.com>
2024-11-04 09:38:37 +0200

    Add files via upload

---
+++ Createtableinpostgrescontainer/table.sql
@@ -0,0 +1,18 @@
+####################################
+#                                  #
+#         Created by Bayoumi       #
+#                                  #
+####################################
+
+
+
+CREATE TABLE smartphones (
+    id INT PRIMARY KEY,
+    brand VARCHAR(50),
+    screen_size DECIMAL(3,2),
+    ram DECIMAL(3,1),
+    rom DECIMAL(4,1),
+    sim_type VARCHAR(10),
+    battery DECIMAL(5,1),
+    price DECIMAL(6,1)
+);
